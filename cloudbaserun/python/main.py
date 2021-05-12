3f839908189e2e212540ebdd0dae6772997b4554
qifengwang <qifengwang@tencent.com>
2021-05-12 13:36:07 +0800

    更新

--- cloudbaserun/python/main.py
+++ cloudbaserun/python/main.py
@@ -1,12 +1,14 @@
 import os
 
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return request.remote_addr
+    #return 'Hello World!'
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
