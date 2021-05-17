9555c74241939312a68338e05ff84a89eb2b33d7
junjieyipai <46878637+junjieyipai@users.noreply.github.com>
2021-05-17 17:15:14 +0800

    Update main.py

--- cloudbaserun/python/main.py
+++ cloudbaserun/python/main.py
@@ -7,9 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    if 'HTTP_X_FORWARDED_FOR' in request.environ:
-        return request.environ['HTTP_X_FORWARDED_FOR'].split(',')[0]
-    return request.remote_addr
+    return 'hello world'
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
