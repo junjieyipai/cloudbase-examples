4e7201d5e4c405087f852e1e20253c733f181f4d
junjieyipai <46878637+junjieyipai@users.noreply.github.com>
2021-05-17 17:59:18 +0800

    Update main.py

--- cloudbaserun/python/main.py
+++ cloudbaserun/python/main.py
@@ -1,5 +1,4 @@
 import os
-import random
 
 from flask import Flask
 from flask import request
@@ -8,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return str(random.randint(1, 55))
+    return 'I want T-shirt'
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
