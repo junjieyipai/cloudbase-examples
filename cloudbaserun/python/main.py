ea2306b73ed17913d7e03dd1ee16e2cf2f95434e
junjieyipai <46878637+junjieyipai@users.noreply.github.com>
2021-05-17 17:37:31 +0800

    Update main.py

--- cloudbaserun/python/main.py
+++ cloudbaserun/python/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return + random.randint(1, 55)
+    return random.randint(1, 55)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
